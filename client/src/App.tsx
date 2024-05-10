9ea5befadff9bd042f58c4513159388f7dece74b
Soham Pathak <soham.pathak@datenwissen.com>
2024-05-10 06:49:40 +0530

    Running vercel build

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,8 @@ import Home from "./pages/Home/index";
 function App() {
   
   // const isLogin = useAuth();       
-  return <>{isLogin ? <Home /> : <span>Unauthorized</span>}</>;
+  // return <>{isLogin ? <Home /> : <span>Unauthorized</span>}</>;
+  return <><Home/></>
 }
 
 export default App;

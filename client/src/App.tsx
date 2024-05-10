02dfd8ea8410b29ab362d6d9a4974b2d6bf60b89
Soham Pathak <soham.pathak@datenwissen.com>
2024-05-10 06:36:20 +0530

    Removed Keycloak

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 //import "./App.css";
-import useAuth from "./hooks/useAuth";
+//import useAuth from "./hooks/useAuth";
 import Creator from "./pages/Canvas.tsx/index.tsx";
 import Home from "./pages/Home/index";
 
 function App() {
-  const isLogin = useAuth();
+  // const isLogin = useAuth();
         
   return <>{isLogin ? <Home /> : <span>Unauthorized</span>}</>;
 }

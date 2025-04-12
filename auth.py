18f082880f8562e84733e189159d99b5aee7f99d
pathaksoham2003 <pathaksoham2003@gmail.com>
2025-04-12 19:57:37 +0530

    Added

--- auth.py
+++
@@ -1,7 +0,0 @@
-def commit_callback(commit):
-    if commit.author_name == b"sohampathakdnw":
-        commit.author_name = b"pathaksoham2003"
-        commit.author_email = b"pathaksoham2003@gmail.com"
-    if commit.committer_name == b"sohampathakdnw":
-        commit.committer_name = b"pathaksoham2003"
-        commit.committer_email = b"pathaksoham2003@gmail.com"
